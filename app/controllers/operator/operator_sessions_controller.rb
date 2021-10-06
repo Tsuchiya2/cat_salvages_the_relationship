30eeca40b40eb300b959eeea574da915970ab49c
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-06 09:33:53 +0900

    [Add] flash_messageを追加する
     ・views/shared/_flash_message.html.slimを追加しました。
     ・views/operator/layouts/application.html.slimから上記を呼び出す記述を追加しました。
         => views/lyaouts/の方には追加しておりません。
         => 理由は一般ユーザーがフラッシュメッセージを受け取る場面が
            現段階の実装では存在しないため、必要になった際に記載する予定です。
     ・controllers/operator/base_controller.rbを一部修正しました。
     ・controllers/operator/operator_sessions_controller.rbを一部修正しました。
     ・views/operator/shared/_navbar.html.slimを一部修正しました。
     ・config/locales/activerecord/ja.ymlに一部追記をしました。

--- app/controllers/operator/operator_sessions_controller.rb
+++ app/controllers/operator/operator_sessions_controller.rb
@@ -7,7 +7,7 @@ class Operator::OperatorSessionsController < Operator::BaseController
     @operator = login(params[:email], params[:password])
 
     if @operator
-      redirect_to operator_boards_path
+      redirect_to operator_boards_path, success: 'キャットインしました。'
     else
       render :new
     end
@@ -15,6 +15,6 @@ class Operator::OperatorSessionsController < Operator::BaseController
 
   def destroy
     logout
-    redirect_to operator_cat_in_path
+    redirect_to operator_cat_in_path, success: 'キャットアウトしました。'
   end
 end
