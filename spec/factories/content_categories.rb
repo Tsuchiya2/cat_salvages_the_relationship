87c881839587bf7946be95adcb94516eaa57b449
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-07 15:48:53 +0900

    [Add] systemテスト(content_categories_spec.rb)を追加する
     ・spec/system/content_categories_spec.rを追加しました。
     ・spec/support/login_macros.rbにてloginメソッドを定義しました。
     ・spec/support/capybara.rbを追加しました。
     ・spec/factories/content_categories.rb一部修正しました。
     ・.rubocop.ymlに追記を行いました。

--- spec/factories/content_categories.rb
+++ spec/factories/content_categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :content_category do
-    name { 'カテゴリー' }
+    name { 'Category' }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,7 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -65,4 +66,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.include LoginMacros
 end
