7bb45ee529052abf3d83f1c2a0ce5249c21379f1
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-08 14:46:57 +0900

    [Add] AlarmContentCategoryを実装する
     ・controllers/operator/以下にコントローラーを作成しました。
     ・views/operator/以下に必要なビューファイルを作成しました。
     ・config/routes.rbに必要なルーティングを設定しました。
     ・views/operator/shared/_navbar.html.slimにアラームカテゴリーを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :boards, only: %i[index]
     resources :content_categories
     resources :contents
+    resources :alarm_content_categories
   end
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
