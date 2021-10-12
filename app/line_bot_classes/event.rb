2acfa4da4d866bedf07a1704a88c54cec8dae792
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-12 14:18:53 +0900

    [Add] グループから発生したイベントを適切に処理する機能を実装する
     ・app/line_bot_classes_event.rb：コメント等修正。
     ・手動にて複数人(3人)トークルームでの挙動確認。
     ・手動にてグループ(3人)グループでの挙動確認。
       ＊上記の人数にはLINE_Botは含まれません。

--- app/line_bot_classes/event.rb
+++ app/line_bot_classes/event.rb
@@ -7,10 +7,10 @@ class Event
     end
   end
 
-  # === 受け取ったイベントによって処理を行います ====
   def self.event_branches(event, client)
     case event
     when Line::Bot::Event::Message
+      Event.goodbye_cat(event, client) if event['message']['type'] == Line::Bot::Event::MessageType::Text
       Event.catch_message(event, client)
     when Line::Bot::Event::Join, Line::Bot::Event::MemberJoined
       Event.join_events(event, client)
@@ -19,12 +19,29 @@ class Event
     end
   end
 
-  # === メンバーがテキスト・スタンプetcを送信した際、remind_at, status を更新します ====
+  # +++++ ↓↓↓ LINE_Bot を退出させるおまじない ↓↓↓ +++++
+  def self.goodbye_cat(event, client)
+    group_id = Event.judge_group_or_room(event)
+    return if group_id.blank?
+
+    Event.cat_go_away(event, group_id, client) if event.message['text'].match?('ニャンダフルライフ')
+  end
+
+  def self.cat_go_away(event, group_id, client)
+    if event['source']['groupId']
+      client.leave_group(group_id)
+    elsif event['source']['roomId']
+      client.leave_room(group_id)
+    end
+  end
+  # +++++ ↑↑↑ LINE_Bot を退出させるおまじない ↑↑↑ +++++
+
+  # ===== ↓↓↓ メンバーがテキスト・スタンプetcを送信した際、remind_at, status を更新します ↓↓↓ =====
   def self.catch_message(event, client)
     group_id = Event.judge_group_or_room(event)
     return if group_id.blank?
 
-    json_data = client.get_group_members_count(group_id)
+    json_data = Event.members_count(event, client)
     count_menbers = JSON.parse(json_data.body)
     return if count_menbers['count'].to_i < 2
 
@@ -32,13 +49,14 @@ class Event
     random_number = (23..60).to_a.sample
     line_group.update!(remind_at: Time.current.since(random_number.days), status: :wait)
   end
+  # ===== ↑↑↑ メンバーがテキスト・スタンプetcを送信した際、remind_at, status を更新します ↑↑↑ =====
 
-  # === メンバー or LINE_Bot が入室した際、人数によっては LineGroup を作成します ===
+  # ***** ↓↓↓ メンバー or LINE_Bot が入室した際、人数によっては LineGroup を作成します ↓↓↓ *****
   def self.join_events(event, client)
     group_id = Event.judge_group_or_room(event)
     return if group_id.blank?
 
-    json_data = client.get_group_members_count(group_id)
+    json_data = Event.members_count(event, client)
     count_menbers = JSON.parse(json_data.body)
     if LineGroup.find_by(line_group_id: group_id).nil? && count_menbers['count'].to_i > 1
       LineGroup.create!(line_group_id: group_id, remind_at: Time.current.since(3.days), status: :call)
@@ -46,23 +64,25 @@ class Event
     message = { type: 'text', text: '〇〇ニャ🐾よろしくニャ🐱🐾' }
     client.push_message(group_id, message)
   end
+  # ***** ↑↑↑ メンバー or LINE_Bot が入室した際、人数によっては LineGroup を作成します ↑↑↑ *****
 
-  # === メンバー or LINE_Bot が退出した際、残った人数によっては LineGroup を削除します ===
+  # ----- ↓↓↓ メンバー or LINE_Bot が退出した際、残った人数によっては LineGroup を削除します ↓↓↓ -----
   def self.leave_events(event, client)
     group_id = Event.judge_group_or_room(event)
     return if group_id.blank?
 
-    json_data = client.get_group_members_count(group_id)
+    json_data = Event.members_count(event, client)
     count_menbers = JSON.parse(json_data.body)
-    return if count_menbers['count'].to_i > 1
+    return if count_menbers['count'].to_i > 1 # LINE_Botが退出した際はclientからの返り値は'{}'で、登録されていないキーに'.to_i'を行うと'0'(nil)になります。
 
     line_group = LineGroup.find_by(line_group_id: group_id)
     line_group.destroy!
   end
+  # ----- ↑↑↑ メンバー or LINE_Bot が退出した際、残った人数によっては LineGroup を削除します ↑↑↑ -----
 
-  # LINE_Bot が加わっている先が グループ or ルーム かを判定。
+  # LINE_Bot が加わっている先が グループ or 複数人トークルーム かを判定。
   # どちらでもない場合は LINE_Bot と ユーザー になるので nil に設定。
-  # (複数人トークはグループトークに統合：LINE みんなの使い方ガイドより)
+  # (複数人トークルームはグループトークに統合：LINE みんなの使い方ガイドより)
   # https://guide.line.me/ja/friends-and-groups/create-groups.html
   def self.judge_group_or_room(event)
     if event['source']['groupId']
@@ -71,4 +91,22 @@ class Event
       event['source']['roomId']
     end
   end
+
+  # LINE_Bot が加わっている先のメンバー数をJSONで取得
+  def self.members_count(event, client)
+    if event['source']['groupId']
+      client.get_group_members_count(event['source']['groupId'])
+    elsif event['source']['roomId']
+      client.get_room_members_count(event['source']['roomId'])
+    end
+  end
+
+  # 機能確認用のオウム返し用コード - 最終的には削除する予定です。
+  # def self.return_text(event, client)
+  #   group_id = Event.judge_group_or_room(event)
+  #   return if group_id.blank?
+
+  #   message = { type: 'text', text: event.message['text'] }
+  #   client.reply_message(event['replyToken'], message)
+  # end
 end
