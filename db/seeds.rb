6e32ba07d9a5068063fa94039dee4a06debbd1a4
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-13 20:36:29 +0900

    [Add] Content関連のseedデータを追記する。
     ・db/seed.rbにContent関連の記載を追加しました。
     ・seed.rb内記述を一部修正しました。
     ・Content(Alarm含む) enum カラム名と重なる部分を修正しました。
     ・上記の修正に伴いconfig/locales/enum/ja.ymlを修正しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,14 @@
 
 # 仮seedデータ
 Operator.create(name: 'guest',
-                email: 'guest@example.com',
-                password: 'password',
-                password_confirmation: 'password',
-                role: 1) unless Operator.find_by(email: 'guest@example.com')
+                email: Rails.application.credentials.guest[:email],
+                password: Rails.application.credentials.guest[:password],
+                password_confirmation: Rails.application.credentials.guest[:password],
+                role: 1) unless Operator.find_by(email: Rails.application.credentials.guest[:email])
+
+
+unless Content.find_by(body: Rails.application.credentials.content[:movie])
+  Content.create(body: 'ニャ〜ニャ〜久しぶりニャ🐱🐾', category: :call)
+  Content.create(body: Rails.application.credentials.content[:movie], category: :movie)
+  Content.create(body: '聞いたところによると日本にはいろんなお茶会があるらしいニャンね🍵 今度、お茶会ならぬ「ちゅ〜る食べ比べ会」を開催してほしいニャ〜🐾', category: :text)
+end
