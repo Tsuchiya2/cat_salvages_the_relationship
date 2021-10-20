48114a13c7fee68df711e9738cd0763a50d98b7a
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-20 17:52:30 +0900

    [Add] フィードバックを投稿できるようにする
     ・config/routes.rb：resources :feedbacks の行を追加。
     ・controllers/feedbacks_controller.rb：中身を追加。
     ・flash_message を一般ページでも使用するため以下のファイルを修正。
        - controllers/operator/base_controller.rb
        - controllers/application_controller.rb
        - views/layouts/application.html.slim
     ・一般ページのナビバー部分を修正。
        - views/shared/_navbar.html.slim
     ・views/feedbacks/new.html.slim を作成＆記載。
     ・config/lacales/activerecord/ja.yml：一部追記。

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,21 @@
 class FeedbacksController < ApplicationController
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.save
+      redirect_to root_path, success: 'フィードバックありがとうございます！'
+    else
+      flash.now[:danger] = '100〜300文字でお願いします！🐾'
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:text)
+  end
 end
