f4753c43d12cfd0968f285db4944e3ec8f1a0a2e
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-27 13:37:47 +0900

    [Add] LineGroupの詳細ページを追加する
     ・views/operator/line_groups/show.html.slim：記述追加。
     ・controllers/operator/line_groups_controller.rb：記述追加。
     ・policies/line_group_policy.rb：作成＆記述。

--- app/controllers/operator/line_groups_controller.rb
+++ app/controllers/operator/line_groups_controller.rb
@@ -2,23 +2,30 @@ class Operator::LineGroupsController < Operator::BaseController
   before_action :set_line_group, only: %i[show edit update destroy]
 
   def index
+    authorize(LineGroup)
+
     @line_groups = LineGroup.all
   end
 
   def show
+    authorize(@line_group)
   end
 
   def edit
+    authorize(@line_group)
   end
 
   def update
+    authorize(@line_group)
   end
 
   def destroy
+    authorize(@line_group)
   end
 
   private
 
   def set_line_group
+    @line_group = LineGroup.find(params[:id])
   end
 end
