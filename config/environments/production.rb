e399e59a89412f3efe5552ca7cbfdf46e03ae2fa
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-10-30 21:04:26 +0900

    [Add] production環境でのメール送信設定を追加する
     ・config/credentials.yml.enc：追記。
     ・config/environments/production.rb：メール設定追記。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'example.com',
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
