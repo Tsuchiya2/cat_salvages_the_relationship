08cfdb18416e66f59195b8bc41d867763c43619f
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-11-02 15:47:57 +0900

    [Fix] Content(Alarm含む)のenumの命名を修正する
     ・上記の変更に伴い必要な箇所の修正を行いました。
     ・rspec, rubocop コマンド結果：オールグリーン状態です。

--- app/models/alarm_content.rb
+++ app/models/alarm_content.rb
@@ -1,5 +1,5 @@
 class AlarmContent < ApplicationRecord
-  enum category: { contact: 0, proposal: 1, url: 2, naive: 3, free: 4 }
+  enum category: { contact: 0, free_one: 1, free_two: 2, free_three: 3, text: 4 }
   validates :body,       presence: true, uniqueness: true, length: { in: 2..255 }
   validates :category,   presence: true
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ApplicationRecord
-  enum category: { call: 0, movie: 1, free: 2 }
+  enum category: { contact: 0, free: 1, text: 2 }
   validates :body,       presence: true, uniqueness: true, length: { in: 2..255 }
   validates :category,   presence: true
 end
