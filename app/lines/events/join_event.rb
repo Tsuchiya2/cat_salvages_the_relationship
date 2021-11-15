a255ba957fedc94e2debba0c494defe66cc72af0
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-11-16 00:25:08 +0900

    [Fix] 各LineEventに'Events::'を追記する

--- app/lines/events/join_event.rb
+++ app/lines/events/join_event.rb
@@ -1,13 +1,13 @@
 module Events::JoinEvent
   def join_bot(client, group_id, count_menbers)
-    LineEvent.create_line_group(group_id, count_menbers)
+    Events::LineEvent.create_line_group(group_id, count_menbers)
     message = { type: 'text',
                 text: '加えてくれてありがとうニャ🌟！！最後のLINEから3週間〜2ヶ月後にwake upのLINEするニャ！！（反応が無いとすぐかも知れニャンよ⏰）末永くよろしくニャ🐱🐾' }
     client.push_message(group_id, message)
   end
 
   def join_member(client, group_id, count_menbers)
-    LineEvent.create_line_group(group_id, count_menbers)
+    Events::LineEvent.create_line_group(group_id, count_menbers)
     message = { type: 'text', text: '初めまして🌟ReLINE(https://www.cat-reline.com/)の"猫さん"っていうニャ🐱よろしくニャ🐾！！' }
     client.push_message(group_id, message)
   end
--- app/lines/events/line_event.rb
+++ app/lines/events/line_event.rb
@@ -12,33 +12,33 @@ class Events::LineEvent
 
   def self.catch_events(events, client)
     events.each do |event|
-      LineEvent.callback_action(event, client)
+      Events::LineEvent.callback_action(event, client)
     rescue StandardError => e
       group_id = Event.catch_group_or_room_id(event)
       error_message = "<Callback> 例外:#{e.class}, メッセージ:#{e.message}, バックトレース:#{e.backtrace}"
-      LineMailer.error_email(group_id, error_message).deliver_later
+      Events::LineEvent.error_email(group_id, error_message).deliver_later
     end
   end
 
   def self.callback_action(event, client)
-    group_id = LineEvent.catch_group_or_room_id(event)
+    group_id = Events::LineEvent.catch_group_or_room_id(event)
     return if group_id.blank?
 
-    json_data = LineEvent.count_group_members(event, client)
+    json_data = Events::LineEvent.count_group_members(event, client)
     count_menbers = JSON.parse(json_data.body)
-    LineEvent.branch_event_type(event, client, group_id, count_menbers)
+    Events::LineEvent.branch_event_type(event, client, group_id, count_menbers)
   end
 
   def self.branch_event_type(event, client, group_id, count_menbers)
     case event
     when Line::Bot::Event::Message
-      LineEvent.catch_message(event, client, group_id, count_menbers)
+      Events::LineEvent.catch_message(event, client, group_id, count_menbers)
     when Line::Bot::Event::Join
-      LineEvent.join_bot(client, group_id, count_menbers)
+      Events::LineEvent.join_bot(client, group_id, count_menbers)
     when Line::Bot::Event::MemberJoined
-      LineEvent.join_member(client, group_id, count_menbers)
+      Events::LineEvent.join_member(client, group_id, count_menbers)
     when Line::Bot::Event::Leave, Line::Bot::Event::MemberLeft
-      LineEvent.leave_events(group_id, count_menbers)
+      Events::LineEvent.leave_events(group_id, count_menbers)
     end
   end
 
